92d71f340d3a5f51aaf5edb37b8056fe3c511598
GiannaDavila <giannagiannadavila@gmail.com>
2022-10-20 15:46:14 -0400

    first commit

--- functions/src/furniture.js
+++ functions/src/furniture.js
@@ -15,3 +15,20 @@ export async function getAllFurniture(req,res) {
     //send back array of furnitures
     res.send(collection)
 }
+
+export async function  addNewFurniture(req,res){
+    // get new furniture from the body of the request 
+    const { brand, model }= req.body
+    const addNewFurniture = {brand, model}
+    //connect to database
+    const db = dbConnect()
+    // put this new furniture into our furniture collection in out db
+    await db.collection ('furniture').insertOne(addNewFurniture)
+    // catch error and send with status 500
+    .catch(err => {
+        res.status(500).send(err)
+        return 
+    })
+    //return response with 201
+    res.status(201).send({message:'furniture added'})
+}
